ca67338bde9d7871c3dff9224ab6002eb11c2e0d
Marbacj <Uncle.YuH@outlook.com>
2023-11-26 10:51:11 +0800

    Site updated: 2023-11-26 10:51:11

---
+++ music/cleanPjax.js
@@ -0,0 +1,8 @@
+$(document).on('pjax:start', function () {
+    if (window.aplayers) {
+        for (let i = 0; i < window.aplayers.length; i++) {
+            window.aplayers[i].destroy();
+        }
+        window.aplayers = [];
+    }
+});
